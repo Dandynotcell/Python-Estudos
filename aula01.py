d1a34ad6eda3f2546c82a6f1b4a3aedcbf1ae8fc
Andersom Ricardo <dandy.notcell@gmail.com>
2026-06-03 22:40:44 -0300

    Minah Aula fundamentos

---
+++ aula01.py
@@ -0,0 +1,21 @@
+print("Olá mundo") 
+nome = "Andersom"
+idade = 40
+altura = 1.60
+programador = True 
+
+
+print(nome)
+print(idade)
+print(altura)
+print(programador)
+print("O nome do programador é "+ "Andersom")
+
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(programador))
+
+nome = "Andersom" 
+idade = 40 
+print(f"meu nome é {nome} e tenho {idade} anos")
