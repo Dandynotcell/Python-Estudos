724216e81532e2b16baf947ea2c689c1b7eccbe6
Andersom Ricardo <dandy.notcell@gmail.com>
2026-06-09 22:47:02 -0300

    aula

---
+++ exercicio2.py
@@ -0,0 +1,6 @@
+# 1. Entrada: o que o usuario ou sistema Fornece?
+nome = input("Digite seu nome")
+# 2. Processamento: que regra será aplicada>
+mensagem = f"ola, {nome}! Bem Vindo Ao Python"
+# 3. Saida: Qual resutado Aparece?
+print (mensagem)
